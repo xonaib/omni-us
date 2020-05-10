2ff12ba3dacd18445d572473b7a71710a074f789
zonaib <zonaib@getfieldforce.com>
2020-05-10 12:52:53 +0500

    befor working on cell types

--- projects/design-lib/src/Interfaces/table-interface.ts
+++ projects/design-lib/src/Interfaces/table-interface.ts
@@ -4,4 +4,15 @@ export interface FFColumnDef {
     isSortable?: boolean;
     columnDef: string;
     displayIndex?: number;
+    cellType?: CellType.text | CellType.select | CellType.currency | CellType.date | CellType.time | CellType.dateTime | CellType.custom
+}
+
+export enum CellType {
+    text = 'text',
+    select= 'select',
+    currency = 'currency',
+    date = 'date',
+    time = 'time',
+    dateTime = 'dateTime',
+    custom = 'custom'
 }
